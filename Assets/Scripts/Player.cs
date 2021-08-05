73dfd3e1c462662fb70a5f3d4753c090b6f60525
sj-jason-liu <sj.jason.liu@gmail.com>
2021-08-05 23:30:10 +0800

    Finished creating jumping animation to Idle and Running & Created attacking animation with sword arc & Created virtual camera that following Player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _jumpForce = 5f;
+    [SerializeField]
+    private float _speed = 3f;
+
+    private bool _grounded;
+    private bool _resetJump = false;
+    
+    private Rigidbody2D _rb;
+    private PlayerAnimation _playerAnim;
+    private SpriteRenderer _sprite;
+    
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _playerAnim = GetComponent<PlayerAnimation>();
+        _sprite = GetComponentInChildren<SpriteRenderer>();
+        if(_sprite == null)
+        {
+            Debug.LogError("Spriterenderer is null!");
+        }
+    }
+
+    void Update()
+    {
+        Movement();
+
+        if (Input.GetMouseButtonDown(0) && IsGrounded() == true)
+        {
+            _playerAnim.Attack();
+        }
+    }
+
+    void Movement()
+    {
+        float horiInput = Input.GetAxisRaw("Horizontal");
+        float move = horiInput * _speed;
+        _playerAnim.Move(horiInput);
+        _grounded = IsGrounded();
+
+        Flip(horiInput);
+        
+        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded() == true)
+        {
+            _playerAnim.Jumping(true);
+            StartCoroutine(ResetJumpRoutine());
+            _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
+        }
+        _rb.velocity = new Vector2(move, _rb.velocity.y);
+    }
+
+    bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.8f, 1 << 8);
+        if (hit.collider != null)
+        {
+            if (_resetJump == false)
+            {
+                _playerAnim.Jumping(false);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void Flip(float facing)
+    {
+        if (facing < 0)
+        {
+            _sprite.flipX = true;
+        }
+        else if (facing > 0)
+        {
+            _sprite.flipX = false;
+        }
+    }
+
+    IEnumerator ResetJumpRoutine()
+    {
+        _resetJump = true;
+        yield return new WaitForSeconds(0.1f);
+        _resetJump = false;
+    }
+}
---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator _anim;
+    private Animator _swordAnim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponentInChildren<Animator>();
+        if(_anim == null)
+        {
+            Debug.LogError("Animator is missing!");
+        }
+        _swordAnim = transform.GetChild(1).GetComponent<Animator>();
+        if(_swordAnim == null)
+        {
+            Debug.LogError("Sword Animation is NULL!");
+        }
+    }
+
+    public void Move(float moveValue)
+    {
+        _anim.SetFloat("Move", Mathf.Abs(moveValue));
+    }
+
+    public void Jumping(bool hasJump)
+    {
+        _anim.SetBool("Jumping", hasJump);
+    }    
+
+    public void Attack()
+    {
+        _anim.SetTrigger("Attacking");
+        _swordAnim.SetTrigger("SwordAnim");
+
+    }
+}
