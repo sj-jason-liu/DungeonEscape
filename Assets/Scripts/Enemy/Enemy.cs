37523e61747b1a277ded530ed6d0ffd52c6fe6b4
sj-jason-liu <sj.jason.liu@gmail.com>
2021-08-07 00:07:30 +0800

    Created abstract class to enemies & moved all the files from Firebase to root folder.

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    protected int health;
+    [SerializeField]
+    protected int speed;
+    [SerializeField]
+    protected int gems;
+
+    public virtual void Attack()
+    {
+
+    }
+
+    public abstract void Update();
+}
