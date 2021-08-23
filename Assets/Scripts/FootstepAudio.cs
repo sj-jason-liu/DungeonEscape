d746bd237c2a0ba392c4018609aeef90074cf381
sj-jason-liu <sj.jason.liu@gmail.com>
2021-08-23 21:51:32 +0800

    Added footstep sound to Player and Moss Giant, and trying to fix the audio distance issue.

---
+++ Assets/Scripts/FootstepAudio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepAudio : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        if (_audioSource == null)
+        {
+            Debug.LogError("AudioSource is NULL!");
+        }
+    }
+
+    public void FootStepSound()
+    {
+        _audioSource.Play();
+    }
+}
